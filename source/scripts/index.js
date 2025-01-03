82e45424bd3ad0a30a52ca9fc6761b6ba114e177
daria-kretova <kretovadar@yandex.ru>
2025-01-03 21:25:40 +0300

    mobile-menu-3

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -22,20 +22,3 @@ navToggle.addEventListener('click', () => {
 // должен пропадать класс .main-nav--opened
 // и появляться класс .main-nav--closed
 // 3. Это цикл
-
-
-// Решение учебеника
-// const navMain = document.querySelector('.main-nav');
-// const navToggle = document.querySelector('.main-nav__toggle');
-
-// navMain.classList.remove('main-nav--nojs');
-
-// navToggle.addEventListener('click', () => {
-//   if (navMain.classList.contains('main-nav--closed')) {
-//     navMain.classList.remove('main-nav--closed');
-//     navMain.classList.add('main-nav--opened');
-//   } else {
-//     navMain.classList.add('main-nav--closed');
-//     navMain.classList.remove('main-nav--opened');
-//   }
-// });
